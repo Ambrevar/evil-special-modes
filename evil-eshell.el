61ec6486d947fd51741791c538a5dc995612c165
Pierre Neidhardt <ambrevar@gmail.com>
2017-10-16 18:14:14 +0100

    Don't use any namespace separator
    Neither Evil-Magit nor Evil-Ediff do.

--- evil-eshell.el
+++ evil-eshell.el
@@ -28,26 +28,26 @@
 (require 'evil)
 (require 'eshell)
 
-(defun evil/eshell-next-prompt ()
+(defun evil-eshell-next-prompt ()
   (when (get-text-property (point) 'read-only)
     ;; If at end of prompt, `eshell-next-prompt' will not move, so go backward.
     (beginning-of-line)
     (eshell-next-prompt 1)))
 
-(defun evil/eshell-setup ()
+(defun evil-eshell-setup ()
   (dolist (hook '(evil-replace-state-entry-hook evil-insert-state-entry-hook))
-    (add-hook hook 'evil/eshell-next-prompt nil t)))
+    (add-hook hook 'evil-eshell-next-prompt nil t)))
 
-(add-hook 'eshell-mode-hook 'evil/eshell-setup)
+(add-hook 'eshell-mode-hook 'evil-eshell-setup)
 
-(defun evil/eshell-interrupt-process ()
+(defun evil-eshell-interrupt-process ()
   (interactive)
   (eshell-interrupt-process)
   (evil-insert 1))
 
 ;;; `eshell-mode-map' is reset when Eshell is initialized in `eshell-mode'. We
 ;;; need to add bindings to `eshell-first-time-mode-hook'.
-(defun evil/eshell-set-keys ()
+(defun evil-eshell-set-keys ()
   (with-eval-after-load 'init-helm
     (evil-define-key 'insert eshell-mode-map "\C-e" 'helm-find-files))
   (evil-define-key 'normal eshell-mode-map
@@ -56,17 +56,16 @@
     (kbd "C-k") 'eshell-previous-prompt
     (kbd "C-j") 'eshell-next-prompt
     "0" 'eshell-bol
-    (kbd "C-c C-c") 'evil/eshell-interrupt-process
     (kbd "<return>") 'eshell-send-input
+    (kbd "C-c C-c") 'evil-eshell-interrupt-process
     (kbd "M-h") 'eshell-backward-argument
     (kbd "M-l") 'eshell-forward-argument)
   (evil-define-key 'insert
     eshell-mode-map (kbd "M-h") 'eshell-backward-argument
     (kbd "M-l") 'eshell-forward-argument))
 
-;; TODO: Compare this setup procedure with evil-ediff.  Decide on a namespace
-;; separator.
-(add-hook 'eshell-first-time-mode-hook 'evil/eshell-set-keys)
+;; TODO: Compare this setup procedure with evil-ediff.
+(add-hook 'eshell-first-time-mode-hook 'evil-eshell-set-keys)
 
 (provide 'evil-eshell)
 ;;; evil-eshell.el ends here
